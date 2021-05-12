8f223cfbd9eabd8d5eaaf86b597b6f743eca3828
ZhangHan <202041404130@dgut.edu.cn>
2021-05-12 21:08:41 +0800

    .

--- Data structure/exercise/1.1.h
+++ Data structure/exercise/1.1.h
@@ -2,187 +2,193 @@
 typedef struct Node
 {
     double a; //系数
-    int b; //指数
+    int b;    //指数
     struct Node *next;
-}SLNode;
+} SLNode;
 void ListInitiate(SLNode **head) //初始化
 {
-    *head=(SLNode *)malloc(sizeof(SLNode));
-    (*head)->next=NULL;
+    *head = (SLNode *)malloc(sizeof(SLNode));
+    (*head)->next = NULL;
 }
-SLNode *ListInsert(SLNode *head,double a,int b) //多项式链表建立，保证有序
+SLNode *ListInsert(SLNode *head, double a, int b) //多项式链表建立，保证有序
 {
-    SLNode *curr,*pre,*q;
-    curr=head->next; //curr指向第一项
-    pre=head; //pre指向头结点
+    SLNode *curr, *pre, *q;
+    curr = head->next; //curr指向第一项
+    pre = head;        //pre指向头结点
     //定位，从第一项开始寻找第一个大于或等于b的元素
-    while(curr!=NULL && curr->b<=b)
+    while (curr != NULL && curr->b <= b)
     {
-        pre=curr;
-        curr=curr->next;
+        pre = curr;
+        curr = curr->next;
     }
     //如果找到与b相等的元素，合并同类项
-    if(pre->b==b)
+    if (pre->b == b)
     {
-        pre->a=pre->a+a;
+        pre->a = pre->a + a;
         return head; //返回头指针
     }
     //插入新的项
-    q=(SLNode *)malloc(sizeof(SLNode));
-    q->a=a;
-    q->b=b;
-    q->next=pre->next;
-    pre->next=q;
+    q = (SLNode *)malloc(sizeof(SLNode));
+    q->a = a;
+    q->b = b;
+    q->next = pre->next;
+    pre->next = q;
     return head;
 }
-SLNode *ListPlus(SLNode *P,SLNode *Q) //多项式相加
+SLNode *ListPlus(SLNode *P, SLNode *Q) //多项式相加
 {
-    SLNode *head,*p,*q;
-    int a,b;
-    head=(SLNode *)malloc(sizeof(SLNode)); //申请新多项式的头结点
-    head->next=NULL;
+    SLNode *head, *p, *q;
+    int a, b;
+    head = (SLNode *)malloc(sizeof(SLNode)); //申请新多项式的头结点
+    head->next = NULL;
     //循环初始化
-    p=head;
-    P=P->next;
-    Q=Q->next;
+    p = head;
+    P = P->next;
+    Q = Q->next;
     //建立新多项式，并保证有序
-    while (P!=NULL && Q!=NULL)
+    while (P != NULL && Q != NULL)
     {
         //从两个多项式的第一项开始比较大小
-        if(P->b<Q->b)
+        if (P->b < Q->b)
         {
-            a=P->a;
-            b=P->b;
-            P=P->next; //P后移，Q不动
+            a = P->a;
+            b = P->b;
+            P = P->next; //P后移，Q不动
         }
-        else if(Q->b<P->b)
+        else if (Q->b < P->b)
         {
-            a=Q->a;
-            b=Q->b;
-            Q=Q->next; //Q后移，P不动
+            a = Q->a;
+            b = Q->b;
+            Q = Q->next; //Q后移，P不动
         }
-        else if(P->b==Q->b) //合并同类项
+        else if (P->b == Q->b) //合并同类项
         {
-            a=P->a+Q->a;
-            b=P->b;
+            a = P->a + Q->a;
+            b = P->b;
             //P,Q均后移
-            P=P->next;
-            Q=Q->next;
+            P = P->next;
+            Q = Q->next;
         }
-        q=(SLNode *)malloc(sizeof(SLNode));
-        q->a=a;
-        q->b=b;
-        q->next=p->next;
-        p->next=q;
-        p=p->next;
+        q = (SLNode *)malloc(sizeof(SLNode));
+        q->a = a;
+        q->b = b;
+        q->next = p->next;
+        p->next = q;
+        p = p->next;
     }
     //若上述循环结束后P或Q中还有元素未放入新多项式，依次放入
-    while (P!=NULL)
+    while (P != NULL)
     {
-        q=(SLNode *)malloc(sizeof(SLNode));
-        q->a=P->a;
-        q->b=P->b;
-        q->next=p->next;
-        p->next=q;
-        p=p->next;
-        P=P->next;
+        q = (SLNode *)malloc(sizeof(SLNode));
+        q->a = P->a;
+        q->b = P->b;
+        q->next = p->next;
+        p->next = q;
+        p = p->next;
+        P = P->next;
     }
-    while (Q!=NULL)
+    while (Q != NULL)
     {
-        q=(SLNode *)malloc(sizeof(SLNode));
-        q->a=Q->a;
-        q->b=Q->b;
-        q->next=p->next;
-        p->next=q;
-        p=p->next;
-        Q=Q->next;
+        q = (SLNode *)malloc(sizeof(SLNode));
+        q->a = Q->a;
+        q->b = Q->b;
+        q->next = p->next;
+        p->next = q;
+        p = p->next;
+        Q = Q->next;
     }
     return head; //返回新多项式的头指针
 }
-SLNode *ListMinus(SLNode *P,SLNode *Q) //多项式相减
+SLNode *ListMinus(SLNode *P, SLNode *Q) //多项式相减
 {
-    SLNode *head,*p,*q;
-    int a,b;
-    head=(SLNode *)malloc(sizeof(SLNode));
-    head->next=NULL;
-    p=head;
-    P=P->next;
-    Q=Q->next;
-    while (P!=NULL && Q!=NULL)
+    SLNode *head, *p, *q;
+    int a, b;
+    head = (SLNode *)malloc(sizeof(SLNode));
+    head->next = NULL;
+    p = head;
+    P = P->next;
+    Q = Q->next;
+    while (P != NULL && Q != NULL)
     {
-        if(P->b<Q->b)
+        if (P->b < Q->b)
         {
-            a=P->a;
-            b=P->b;
-            P=P->next;
+            a = P->a;
+            b = P->b;
+            P = P->next;
         }
-        else if(Q->b<P->b)
+        else if (Q->b < P->b)
         {
-            a=-Q->a; //将多项式Q中所有系数的相反数放入新多项式
-            b=Q->b;
-            Q=Q->next;
+            a = -Q->a; //将多项式Q中所有系数的相反数放入新多项式
+            b = Q->b;
+            Q = Q->next;
         }
-        else if(P->b==Q->b)
+        else if (P->b == Q->b)
         {
-            a=P->a-Q->a; //同类项相减
-            b=P->b;
-            P=P->next;
-            Q=Q->next;
+            a = P->a - Q->a; //同类项相减
+            b = P->b;
+            P = P->next;
+            Q = Q->next;
         }
-        q=(SLNode *)malloc(sizeof(SLNode));
-        q->a=a;
-        q->b=b;
-        q->next=p->next;
-        p->next=q;
-        p=p->next;
+        q = (SLNode *)malloc(sizeof(SLNode));
+        q->a = a;
+        q->b = b;
+        q->next = p->next;
+        p->next = q;
+        p = p->next;
     }
-    while (P!=NULL)
+    while (P != NULL)
     {
-        q=(SLNode *)malloc(sizeof(SLNode));
-        q->a=P->a;
-        q->b=P->b;
-        q->next=p->next;
-        p->next=q;
-        p=p->next;
-        P=P->next;
+        q = (SLNode *)malloc(sizeof(SLNode));
+        q->a = P->a;
+        q->b = P->b;
+        q->next = p->next;
+        p->next = q;
+        p = p->next;
+        P = P->next;
     }
-    while (Q!=NULL)
+    while (Q != NULL)
     {
-        q=(SLNode *)malloc(sizeof(SLNode));
-        q->a=-Q->a; //将多项式Q中所有系数的相反数放入新多项式
-        q->b=Q->b;
-        q->next=p->next;
-        p->next=q;
-        p=p->next;
-        Q=Q->next;
+        q = (SLNode *)malloc(sizeof(SLNode));
+        q->a = -Q->a; //将多项式Q中所有系数的相反数放入新多项式
+        q->b = Q->b;
+        q->next = p->next;
+        p->next = q;
+        p = p->next;
+        Q = Q->next;
     }
     return head;
 }
 void ListPrint(SLNode *head) //多项式的输出
 {
     SLNode *p;
-    int i=0; //i用于判断多项式是否为0
-    for(p=head->next;p!=NULL;p=p->next)
+    int i = 0; //i用于判断多项式是否为0
+    for (p = head->next; p != NULL; p = p->next)
     {
-        if(p->a==0) continue; //如果系数为0，继续下一次循环
-        if(i==1) printf("+");
-        i=1; //i=1，多项式不为0
-        if(p->a<0) printf("(%.0lf)",p->a); //若系数小于0，系数需带上括号
-        if(p->a>0) printf("%.0lf",p->a);
-        if(p->b!=0) printf("x%d",p->b); //若指数为0，不输出x
+        if (p->a == 0)
+            continue; //如果系数为0，继续下一次循环
+        if (i == 1)
+            printf("+");
+        i = 1; //i=1，多项式不为0
+        if (p->a < 0)
+            printf("(%.0lf)", p->a); //若系数小于0，系数需带上括号
+        if (p->a > 0)
+            printf("%.0lf", p->a);
+        if (p->b != 0)
+            printf("x%d", p->b); //若指数为0，不输出x
     }
-    if(i==0) printf("0"); //若多项式为0，输出0
+    if (i == 0)
+        printf("0"); //若多项式为0，输出0
     printf("\n");
 }
 int Destroy(SLNode **head)
 {
-    SLNode *p,*p1;
-    p=*head;
-    while (p!=NULL)
+    SLNode *p, *p1;
+    p = *head;
+    while (p != NULL)
     {
-        p1=p;
-        p=p->next;
+        p1 = p;
+        p = p->next;
         free(p1);
     }
-    *head=NULL;
+    *head = NULL;
 }
--- Data structure/exercise/test.c
+++ Data structure/exercise/test.c
@@ -2,12 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-void fun(char BT[])
+void enterStr(char *str, int num) //输入字符串
 {
-	int max=strlen(BT);
+    int i = 0;
+    char tmp[10];
+    while (1)
+    {
+        if ((tmp[i] = getchar()) == '\n' || i >= num - 1)
+            break;
+        i++;
+    }
+    tmp[i] = '\0';
+    strcpy(str, tmp);
 }
+
 int main()
 {
-	char BT[]={'A','B','C','^','D','E','^','^','^','F','^','^','G','\0'};
-	fun(BT);
+    char str[5];
+    enterStr(str, 5);
+    printf("%s", str);
 }
